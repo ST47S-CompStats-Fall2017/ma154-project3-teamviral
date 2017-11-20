c7d5da30420b2a118812b976f89913973a520217
Sal (Wanying) Fu <wanying.fu@pomona.edu>
2017-11-19 21:37:42 -0800

    retrieved more explanatory variables

--- reddit_api.py
+++ reddit_api.py
@@ -23,20 +23,42 @@ reddit = praw.Reddit(client_id='rNGwNS4hRPJDfw', client_secret="WAe0pEENwYhWu6tE
 
 #print(reddit.user.me())
 
-# Columns
-id_list = []
-url_list = []
-title_list = []
-score_list = []
-
 #for submission in reddit.front.hot(limit=10):
 
 ## Convert to epoch timestamp via https://www.epochconverter.com/
 # The 2 month interval here gives ~1900 rows of data
 # Sept 20, 2017
-start_time = 1505878887
+start_time = 1511149287 - 1000000
 # Nov 20, 2017
 end_time = 1511149287
+
+#this is the list of dictionary names that we can use to pull data from vars(submission)
+
+#domain
+#link_flair_text = categorization of the article (sub science field)
+#author - Reddit object
+#author_flair_css_class
+#ups: total upvotes(?)
+#created_utc (time of creation)
+#num_comments
+#author_flair_text
+#gilded (somebody thought it was worth spending money on this)
+#preview
+
+# Columns
+id_list = []
+url_list = []
+title_list = []
+score_list = []
+domain_list = []
+subfield_list = []
+author_list = []
+author_flair_list = []
+total_upvotes_list = []
+created_utc_list = []
+num_comments_list = []
+gilded_list = []
+
 
 ## Searching posts from a given period of time in the SCIENCE subreddit
 for submission in reddit.subreddit('science').submissions(
@@ -48,12 +70,29 @@ for submission in reddit.subreddit('science').submissions(
     title_list.append(submission.title.encode('utf-8'))
     score_list.append(submission.score)
 
+    domain_list.append(vars(submission)['domain'])
+    subfield_list.append(vars(submission)['link_flair_text'])
+    author_list.append(vars(submission)['author'])
+    author_flair_list.append(vars(submission)['author_flair_css_class'])
+    total_upvotes_list.append(vars(submission)['ups'])
+    created_utc_list.append(vars(submission)['created_utc'])
+    num_comments_list.append(vars(submission)['num_comments'])
+    gilded_list.append(vars(submission)['gilded'])
+
 # Consolidate lists into one pandas data frame
 reddit_df = pd.DataFrame(
     {'id': id_list,
      'url': url_list,
      'title': title_list,
      'score': score_list,
+     'domain': domain_list,
+     'subfield': subfield_list,
+     'author': author_list,
+     'author_flair': author_flair_list,
+     'upvotes': total_upvotes_list,
+     'created_utc': created_utc_list,
+     'num_comments': num_comments_list,
+     'gilded': gilded_list,
     })
 
 # Check out the dimensions of the df
